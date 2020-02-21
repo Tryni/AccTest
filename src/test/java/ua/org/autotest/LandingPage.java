c260394a61fc178e91a47e521f3fa12a37efdc79
olenatrunova <olena.trunova@ringteam.com>
2020-02-21 12:14:50 +0200

    1st test

--- src/test/java/ua/org/autotest/LandingPage.java
+++ src/test/java/ua/org/autotest/LandingPage.java
@@ -6,27 +6,34 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import org.testng.annotations.*;
+
 import java.util.concurrent.TimeUnit;
 
 public class LandingPage {
-    public static void main(String[] args) throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver","C:\\WebDrivers\\chromedriver.exe");
-        WebDriver driver= new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        driver.get("https://widr.app/en");
-        driver.manage().window().maximize();
-        WebElement element=driver.findElement(By.xpath("//div[@class='jsx-2711542876 wdr-input-container']"));
-        element.click();
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        driver.navigate().back();
-        driver.navigate().forward();
-        Thread.sleep(5000);
-      driver.findElement(By.xpath("//div/button[1]']")).sendKeys(Keys.ENTER);
+    //public static void main(String[] args) throws InterruptedException {
+    // System.setProperty("webdriver.chrome.driver","/Users/olenatrunova/Downloads/chromedriver");
+    public  String baseUrl = "https://accfarm.com/";
+    public WebDriver driver;
 
-      //driver.sendKeys(Keys.ENTER);
-       // driver.quit();
-//implicitWait
-
+    @BeforeTest
+public void setBaseUrl(){
+    driver=new ChromeDriver();
+    driver.get(baseUrl);
     }
 
+@Test
+public void VerifyHomepageTitle(){
+    String expectedTitle="Verified Social Media Accounts on Sale";
+    String actualTitle =driver.getTitle();
+    Assert.assertEquals(actualTitle,expectedTitle);
 }
+@AfterTest
+public void endSession(){
+    driver.quit();}
+}
+
